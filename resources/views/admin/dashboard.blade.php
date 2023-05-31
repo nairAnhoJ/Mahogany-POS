3f34fc0292f7b8558f7a86907306094a936c77e9
John Arian <malondrasarian@gmail.com>
2023-05-31 13:44:12 +0800

    Update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,13 +32,13 @@
 
     @section('page_title', 'DASHBOARD')
     
-    <div class="p-3 lg:ml-64 lg:pt-3 h-screen">
+    <div class="p-3 lg:ml-64 lg:pt-3 h-auto">
         @csrf
-        <div id="contentDiv" class="p-2 w-full flex">
-            <div class="bg-white shadow-md rounded-lg p-3 w-[calc(100%-33vh)]">
+        <div id="contentDiv" class="p-2 w-full flex flex-col lg:flex-row">
+            <div class="bg-white shadow-md rounded-lg p-3 lg:w-[calc(100%-33vh)]">
                 <div class="flex justify-end flex-row lg:justify-between mb-3">
                     <div class="hidden lg:block col-span-2 text-2xl">Overview</div>
-                    <div class="justify-self-end flex items-center">
+                    <div class="justify-self-end flex items-center w-72">
                         <label for="timeframe" class="block text-sm font-medium text-gray-900 mr-4">Timeframe: </label>
                         <select id="timeframe" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full sm:w-80 p-2.5">
                             <option selected value="1">Daily</option>
@@ -50,19 +50,19 @@
                 <div class="lg:grid lg:grid-cols-3 lg:gap-2 space-y-2 lg:space-y-0">
                     <div>
                         <div class="block lg:max-w-sm p-6 bg-emerald-400 border border-gray-200 rounded-lg shadow md:flex md:justify-between lg:block">
-                            <h5 class="mb-2 text-base font-medium tracking-tight text-gray-900 md:text-lg md:self-center md:mb-0">Total Profit</h5>
+                            <h5 class="mb-2 text-base font-medium tracking-tight text-gray-900 sm:text-xl lg:text-sm xl:text-base md:self-center md:mb-0">Total Profit</h5>
                             <p class="font-bold text-gray-700 text-2xl md:text-3xl md:self-center lg:text-2xl">₱ <span id="profit">{{$profit}}</span></p>
                         </div>
                     </div>
                     <div>
                         <div class="block lg:max-w-sm p-6 bg-sky-400 border border-gray-200 rounded-lg shadow md:flex md:justify-between lg:block">
-                            <h5 class="mb-2 text-base font-medium tracking-tight text-gray-900 md:text-lg md:self-center md:mb-0">Total Sales</h5>
+                            <h5 class="mb-2 text-base font-medium tracking-tight text-gray-900 sm:text-xl lg:text-sm xl:text-base md:self-center md:mb-0">Total Sales</h5>
                             <p class="font-bold text-gray-700 text-2xl md:text-3xl md:self-center lg:text-2xl">₱  <span id="sales">{{$sales}}</span></p>
                         </div>
                     </div>
                     <div>
                         <div class="block lg:max-w-sm p-6 bg-red-400 border border-gray-200 rounded-lg shadow md:flex md:justify-between lg:block">
-                            <h5 class="mb-2 text-base font-medium tracking-tight text-gray-900 md:text-lg md:self-center md:mb-0">Total Expenses</h5>
+                            <h5 class="mb-2 text-base font-medium tracking-tight text-gray-900 sm:text-xl lg:text-sm xl:text-base md:self-center md:mb-0">Total Expenses</h5>
                             <p class="font-bold text-gray-700 text-2xl md:text-3xl md:self-center lg:text-2xl">₱  <span id="expenses">{{$expenses}}</span></p>
                         </div>
                     </div>
@@ -71,16 +71,16 @@
                     </div>
                 </div>
             </div>
-            <div class="w-[calc(33vh-20px)] ml-5 flex flex-col gap-5">
-                <div class="bg-white shadow-md rounded-lg w-full h-1/3 flex items-center p-4 flex-col">
-                    <span class="font-bold text-xl mt-5">OCCUPIED TABLE</span>
-                    <span class="mt-5 text-8xl font-bold text-emerald-600">{{ $table->count() }}</span>
+            <div class="lg:w-[calc(33vh-20px)] mt-5 mb-4 lg:my-0 lg:ml-5 flex flex-col gap-5">
+                <div class="bg-white shadow-md rounded-lg w-full aspect-square lg:h-1/3 flex items-center justify-center lg:justify-start p-4 flex-col">
+                    <span class="font-bold text-2xl sm:text-4xl lg:text-sm xl:text-xl 2xl:text-2xl mt-5">OCCUPIED TABLE</span>
+                    <span class="mt-5 text-9xl sm:text-[256px] lg:text-6xl xl:text-8xl 2xl:text-9xl font-bold text-emerald-600">{{ $table->count() }}</span>
                 </div>
-                <div class="bg-white shadow-md rounded-lg w-full h-1/3 flex items-center p-4 flex-col">
-                    <span class="font-bold text-xl mt-5">LOW STOCK ITEMS</span>
-                    <span class="mt-5 text-8xl font-bold text-red-700">{{ $LSICount }}</span>
+                <div class="bg-white shadow-md rounded-lg w-full aspect-square lg:h-1/3 flex items-center justify-center lg:justify-start p-4 flex-col">
+                    <span class="font-bold text-2xl sm:text-4xl lg:text-sm xl:text-xl 2xl:text-2xl mt-5">LOW STOCK ITEMS</span>
+                    <span class="mt-5 text-9xl sm:text-[256px] lg:text-6xl xl:text-8xl 2xl:text-9xl font-bold text-red-700">{{ $LSICount }}</span>
                 </div>
-                <div class="bg-white shadow-md rounded-lg w-full h-1/3 flex items-center p-4 flex-col">
+                <div class="hidden lg:flex bg-white shadow-md rounded-lg w-full lg:h-1/3 items-center p-4 flex-col">
                     <span class="font-bold text-xl mt-5"></span>
                     
                 </div>
