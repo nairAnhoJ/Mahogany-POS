52378ffc5fa7085287e0550cae125235b1e343df
John Arian <malondrasarian@gmail.com>
2023-07-26 11:32:39 +0800

    Update

--- resources/views/admin/reports/summary_sec.blade.php
+++ resources/views/admin/reports/summary_sec.blade.php
@@ -65,13 +65,13 @@
                                                             {{ date('M d, Y', $feCDate) }}
                                                         </th>
                                                         <td class="px-6 py-1 text-center whitespace-nowrap">
-                                                            ₱ {{ $result['stotal'] }}.00
+                                                            ₱ {{ number_format($result['stotal'], 2, '.', ',') }}
                                                         </td>
                                                         <td class="px-6 py-1 text-center whitespace-nowrap">
-                                                            ₱ {{ $result['etotal'] }}.00
+                                                            ₱ {{ number_format($result['etotal'], 2, '.', ',') }}
                                                         </td>
                                                         <td class="px-6 py-1 text-center whitespace-nowrap font-semibold {{ ($result['stotal'] - $result['etotal']) < 0 ? 'text-red-500' : 'text-emerald-500' }}">
-                                                            ₱ {{ $result['stotal'] - $result['etotal'] }}.00
+                                                            ₱ {{ number_format($result['stotal'] - $result['etotal'], 2, '.', ',') }}
                                                         </td>
                                                     </tr>
                                                     @php
